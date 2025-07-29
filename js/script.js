b499d44104844e9154fc5056f6daff889357d6c7
mailomdesign <mail.om.design@gmail.com>
2025-07-29 09:09:38 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 fetch('cases.json')
   .then(response => response.json())
   .then(cases => {
@@ -20,3 +21,4 @@ fetch('cases.json')
   .catch(err => {
     console.error('Ошибка загрузки кейсов:', err);
   });
+*/
